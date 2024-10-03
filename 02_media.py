64e4ee3628763b74599b32b05f2e585970436ce1
WilitonGomes <wiliton.ito@gmail.com>
2024-10-03 20:02:12 -0300

    First programming logic exercises

---
+++ 02_media.py
@@ -0,0 +1,15 @@
+#Título: Calcula média          #Comentario em linha
+'''Autor: Wiliton              
+Data: 19/09/2024
+Descrição: Desenvolver um algorítimo que calcule a média aritimética de 4 notas e apresente para o usuário
+'''
+# Entrda de dados
+nota1 = 10
+nota2 = 1
+nota3 = 6
+nota4 = 8
+# processamento de dados
+soma_notas = nota1 + nota2 + nota3 + nota4 
+media = soma_notas/4
+# saida de dados
+print('Sua média é', media , 'mas pode melhora caso queira')
